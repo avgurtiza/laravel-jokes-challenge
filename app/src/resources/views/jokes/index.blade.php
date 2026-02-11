2326aacad9ed97ee26f539ecdd1f794346e83d58
Slide Gurtiza <slide.gurtiza@gmail.com>
2026-02-11 23:12:45 +0800

    fix: reverts button compenent

--- app/src/resources/views/jokes/index.blade.php
+++ app/src/resources/views/jokes/index.blade.php
@@ -13,13 +13,13 @@
 
     <div id="jokes-container" class="space-y-6 mb-6"></div>
 
-    <button
+    <x-bladewind::button
         id="refresh-btn"
+        color="blue"
         onclick="refreshJokes()"
-        class="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
     >
         Refresh Jokes
-    </button>
+    </x-bladewind::button>
 
     <script>
         function refreshJokes() {
